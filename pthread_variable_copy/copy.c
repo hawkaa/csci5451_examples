202219eaba583decda0db825906bc03dc56bbdb9
Håkon Åmdal <hakon@aamdal.com>
2015-02-04 11:56:56 -0600

    copy argument example

---
+++ pthread_variable_copy/copy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 8
+
+void *
+thread_report(void *s)
+{
+  int *arg = s;
+  sleep(1);
+  printf("Address of argument: %x\n", arg);
+  printf("Value of argument: %i\n", *arg);
+}
+
+int
+main(int argc, char **argv)
+{
+  int i, j;
+  pthread_t threads[NUM_THREADS];
+
+  /* fork */
+  for (i = 0; i < NUM_THREADS; ++i) {
+    pthread_create(&threads[i], NULL, thread_report, &i); 
+  }
+
+  /* join */
+  for (j = 0; j < NUM_THREADS; ++j) {
+    pthread_join(threads[j], NULL);
+  }
+  pthread_exit(NULL);
+}
